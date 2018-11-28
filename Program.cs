15d02039d815d289f55c25316625edbc6c4430ef
kikeles <39755139+kikeles@users.noreply.github.com>
2018-11-28 09:53:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace SegundoParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool encontrado;
+            StreamWriter archivoW;
+            StreamReader archivoR;
+            int opcion =0;
+            char inOut;
+            string cadena = "";
+            try
+            {
+                Console.Write("Menu \n1- Escribir en un archivo\n2 - Leer numeros y ordenar\n3 - Leer archivo\nOpcion:");
+                opcion = int.Parse(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1://ecribir en el archivo
+                        do
+                        {
+                            archivoW = File.AppendText("archivo.txt");
+                            Console.WriteLine("Ingresa texto:");
+                            cadena = Console.ReadLine();
+                            archivoW.WriteLine(cadena);
+                            archivoW.Close();
+                            Console.WriteLine("Deseas agregar mas texto en el archivo (s/n)");
+                            inOut = Convert.ToChar(Console.ReadLine());
+                        } while (inOut == 's');
+                        //archivoW.Close();
+                        break;
+                    case 2:
+
+                        break;
+                    case 3://leer el archivo
+                        archivoR = File.OpenText("archivo.txt");
+                        cadena = archivoR.ReadLine();
+                        while (cadena!=null )
+                        {
+                            Console.WriteLine(cadena);
+                            cadena = archivoR.ReadLine();
+                        }
+                        archivoR.Close();
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("ERROR " + e.Message);
+
+            }
+
+        }
+    }
+}
